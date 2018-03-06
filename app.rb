db99d848531ba56dee4638af7da2aa62fd02cfda
Michael Camilleri <mike@inqk.net>
2018-03-06 17:52:07 +0900

    Add basic authentication.

---
+++ app.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+require 'yaml'
+
+require_relative 'lib/clients'
+require_relative 'lib/authentication'
+
+require_relative 'lib/routes'
---
+++ lib/routes.rb
@@ -0,0 +1,26 @@
+post '/api/1.0/client/:id/:command' do
+  msg = "ID not an integer"
+  return halt 500, msg unless params[:id].to_i
+
+  id = params[:id].to_i
+
+  msg = "No client exists with that ID"
+  return halt 500, msg unless Kyuji::Clients.exists? id
+
+  client = Kyuji::Clients[id]
+  request.body.rewind
+
+  msg = "Authentication failed"
+  return halt 500, msg unless authenticated?(client['method'],
+                                             client,
+                                             request)
+
+  # command_path = client['site_dir'] + '/' +
+  #                client['command_dir'] + '/' +
+  #                params[:command]
+  # pid = spawn "#{command_path}"
+  # Process.detach pid
+
+  msg = "Success"
+  return 200, msg
+end
