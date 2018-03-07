c2b4f5ca3de96dc7131246fe4ca146be378ac691
Michael Camilleri <mike@inqk.net>
2018-03-07 15:34:09 +0900

    Update for deployment with Puma.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
+gem 'puma', '~>3.0'
 gem 'sinatra',  '~>2.0'
--- app.rb
+++ app.rb
@@ -4,4 +4,39 @@ require 'yaml'
 require_relative 'lib/clients'
 require_relative 'lib/authentication'
 
-require_relative 'lib/routes'
+configure {
+  set :server, :puma
+}
+
+class Pumatra < Sinatra::Base 
+  post '/api/1.0/client/:id/:command' do
+    msg = "ID not an integer"
+    return halt 500, msg unless params[:id].to_i
+
+    id = params[:id].to_i
+
+    msg = "No client exists with that ID"
+    return halt 500, msg unless Kyuji::Clients.exists? id
+
+    client = Kyuji::Clients[id]
+    request.body.rewind
+
+    msg = "Authentication failed"
+    return halt 500, msg unless authenticated?(client['method'], client, request)
+
+    command_path = client['site_dir'] + '/' +
+                   client['command_dir'] + '/' +
+                   params[:command]
+
+    msg = "Command not found"
+    return halt 500, msg unless File.file? command_path
+
+    # pid = spawn "#{command_path}"
+    # Process.detach pid
+
+    msg = "Success"
+    return 200, msg
+  end
+
+  run! if app_file == $0
+end
